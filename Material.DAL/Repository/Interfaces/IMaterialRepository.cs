2aef5b276c015ef7ed78ea7c3d3b7f1ca927465b
Ігор Колобилін <igor.kolobulin@gmail.com>
2024-12-08 21:20:42 +0200

    added repository's logic

--- Material.DAL/Repository/Interfaces/IMaterialRepository.cs
+++ Material.DAL/Repository/Interfaces/IMaterialRepository.cs
@@ -4,5 +4,14 @@ namespace Material.DAL.Repository.Interfaces;
 
 public interface IMaterialRepository : IBasicRepository<MaterialEntity>
 {
+    Task CreateMaterial(MaterialEntity material, CancellationToken cancellationToken = default);
+
+    Task<MaterialEntity?> FindMaterialAsync(int id, CancellationToken cancellationToken = default);
+
+    Task UpdateMaterialAsync(MaterialEntity material, CancellationToken cancellationToken = default);
+
+    Task DeleteMaterialAsync(MaterialEntity material, CancellationToken cancellationToken = default);
     
+    Task RemoveMaterialAsync(MaterialEntity material, CancellationToken cancellationToken = default);
+    Task AddMaterialAsync(MaterialEntity material, CancellationToken cancellationToken = default);
 }
